cc0ea239f4a0d7b135f0adf427b98a6b606b2338
Alexandr Zaycev <hunter290592@yandex.ru>
2023-05-30 19:15:36 +0300

    Create main page - Create main page - Create modal window with comments

---
+++ src/components/ModalWindow.tsx
@@ -0,0 +1,44 @@
+import { Button, Modal } from 'react-bootstrap';
+import { CommentResponse } from '../models/response/CommentResponse';
+import CommentItem from './CommentItem';
+
+interface IModalWindow {
+	comments: CommentResponse[] | undefined;
+	show: boolean;
+	onHide: () => void;
+}
+
+const ModalWindow = (props: IModalWindow) => {
+	return (
+		<Modal
+			{...props}
+			size="lg"
+			aria-labelledby="contained-modal-title-vcenter"
+			centered
+		>
+			<Modal.Header closeButton>
+				<Modal.Title id="contained-modal-title-vcenter">
+					Комментарии
+				</Modal.Title>
+			</Modal.Header>
+			<Modal.Body
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+					gap: '10px',
+					paddingInline: '20px',
+				}}
+			>
+				{props.comments &&
+					props.comments.map((item) => (
+						<CommentItem item={item} key={item.id} />
+					))}
+			</Modal.Body>
+			<Modal.Footer>
+				<Button onClick={props.onHide}>Закрыть</Button>
+			</Modal.Footer>
+		</Modal>
+	);
+};
+
+export default ModalWindow;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,25 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { userAPI } from '../services/UserService';
 import { postAPI } from '../services/PostsService';
+import { commentAPI } from '../services/CommentService';
+import postReducer from './slices/PostSlice';
 
 const rootReducer = combineReducers({
+	postReducer,
 	[userAPI.reducerPath]: userAPI.reducer,
 	[postAPI.reducerPath]: postAPI.reducer,
+	[commentAPI.reducerPath]: commentAPI.reducer,
 });
 
 export const store = configureStore({
 	reducer: rootReducer,
 	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware().concat(userAPI.middleware, postAPI.middleware),
+		getDefaultMiddleware().concat(
+			userAPI.middleware,
+			postAPI.middleware,
+			commentAPI.middleware
+		),
 });
 
-export default store;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
